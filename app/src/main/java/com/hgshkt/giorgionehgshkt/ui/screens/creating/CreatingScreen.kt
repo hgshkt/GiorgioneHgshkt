b7b5e0f6fb7f86ed86718a04bf15a8a24653a032
hgshkt <nichtojestvozxc@gmail.com>
2023-06-21 19:12:17 +0300

    Creating screen display correctly

--- app/src/main/java/com/hgshkt/giorgionehgshkt/ui/screens/creating/CreatingScreen.kt
+++ app/src/main/java/com/hgshkt/giorgionehgshkt/ui/screens/creating/CreatingScreen.kt
@@ -4,29 +4,32 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.hgshkt.giorgionehgshkt.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -46,54 +49,66 @@ fun CreatingScreen() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Row {
-            Image(painter = rememberVectorPainter(image = Icons.Default.Close),
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.1f)
+                .background(Color.Gray),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = rememberVectorPainter(image = Icons.Default.Close),
                 contentDescription = "Upload image",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(300.dp)
-                    .clip(shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp))
+                    .fillMaxWidth(0.1f)
+                    .fillMaxHeight()
                     .clickable {
                         viewModel.close()
                     },
-                contentScale = ContentScale.FillWidth)
+                contentScale = ContentScale.FillWidth
+            )
 
-            Image(painter = rememberVectorPainter(image = Icons.Default.Send),
+            Image(
+                painter = rememberVectorPainter(image = Icons.Default.Send),
                 contentDescription = "Upload image",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(300.dp)
-                    .clip(shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp))
+                    .fillMaxWidth(0.1f)
+                    .fillMaxHeight()
                     .clickable {
                         viewModel.uploadPublication()
                     },
-                contentScale = ContentScale.FillWidth)
+                contentScale = ContentScale.FillWidth
+            )
         }
-        Image(
-            painter = rememberVectorPainter(image = Icons.Default.ArrowDropDown),
-            contentDescription = "Upload image",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp)
-                .clip(shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp))
-                .clickable {
-                    launcher.launch("image/*")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.add_photo_gallery),
+                contentDescription = "Upload image",
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight(0.4f)
+                    .border(width = 2.dp, color = Color.DarkGray)
+                    .clickable {
+                        launcher.launch("image/*")
+                    },
+                contentScale = ContentScale.FillHeight
+            )
+
+            TextField(
+                value = text.value,
+                onValueChange = {
+                    text.value = it
                 },
-            contentScale = ContentScale.FillWidth
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = text.value,
-            onValueChange = {
-                text.value = it
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .padding(horizontal = 16.dp),
-            textStyle = MaterialTheme.typography.bodyLarge
-        )
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.9f)
+                    .padding(horizontal = 16.dp),
+                textStyle = MaterialTheme.typography.bodyLarge
+            )
+        }
     }
 }
